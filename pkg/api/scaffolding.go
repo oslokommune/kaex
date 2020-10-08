e7d6ece197f6721680fe205fbffcb84c56d6423e
Julius Pedersen <deifyed@ctemplar.com>
2020-10-08 10:49:57 +0200

    ✅ Add support for templating

--- pkg/api/scaffolding.go
+++ pkg/api/scaffolding.go
@@ -5,84 +5,11 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"os"
 	"path"
 	"time"
 )
-
-const (
-	REPO_EXAMPLE_BASE_URL = "https://raw.githubusercontent.com/oslokommune/kaex/master/examples"
-)
-
-func FetchMinimalExample(w io.Writer) error {
-	exampleFileName := "application-minimal.yaml"
-
-	err := fetchExample(w, exampleFileName)
-	if err != nil {
-		err = fmt.Errorf("unable to fetch %s: %w", exampleFileName, err)
-
-		return err
-	}
-
-	return nil
-}
-
-func FetchFullExample(w io.Writer) error {
-	exampleFileName := "application-full.yaml"
-
-	err := fetchExample(w, exampleFileName)
-	if err != nil {
-		err = fmt.Errorf("unable to fetch %s: %w", exampleFileName, err)
-
-		return err
-	}
-
-	return nil
-}
-
-func fetchLocalExample(name string) (string, error) {
-	configPath, err := os.UserConfigDir()
-	if err != nil {
-		return "", err
-	}
-
-	targetPath := path.Join(configPath, fmt.Sprintf("kaex/%s", name))
-	_, err = os.Stat(targetPath)
-	if err != nil {
-		err = fmt.Errorf("no such file/path \"%s\": %w", targetPath, err)
-
-		return "", err
-	}
-
-	data, err := ioutil.ReadFile(targetPath)
-	if err != nil {
-		err = fmt.Errorf("error while reading file: %w", err)
-
-		return "", err
-	}
-
-	return string(data), nil
-}
-
-func fetchExample(w io.Writer, name string) error {
-	example, err := fetchLocalExample(name)
-	if err == nil {
-		fmt.Fprintf(w, example)
-
-		return nil
-	}
-
-	url := fmt.Sprintf("%s/%s", REPO_EXAMPLE_BASE_URL, name)
-
-	example, err = fetchURL(url)
-	if err != nil {
-		return err
-	}
-
-	fmt.Fprintf(w, example)
-
-	return nil
-}
 
 func fetchURL(url string) (string, error) {
 	spaceClient := http.Client{
@@ -102,10 +29,96 @@ func fetchURL(url string) (string, error) {
 		defer res.Body.Close()
 	}
 
+	if res.StatusCode != 200 {
+		return "", fmt.Errorf("%s not found", url)
+	}
+
 	body, readErr := ioutil.ReadAll(res.Body)
 	if readErr != nil {
 		return "", readErr
 	}
 
 	return string(body), nil
+}
+
+func fetchLocalTemplate(kaex Kaex, writer io.Writer, name string) error {
+	templatesPath := path.Join(kaex.ConfigPath, "templates")
+
+	mainPath := path.Join(templatesPath, name)
+	alternativePath := path.Join(templatesPath, fmt.Sprintf("%s.yaml", name))
+
+	var targetPath string
+	_, mainPathErr := os.Stat(mainPath)
+	_, alternativePathErr := os.Stat(alternativePath)
+
+	if mainPathErr != nil && alternativePathErr != nil {
+		return fmt.Errorf("could not find %s/%s locally: %w", mainPath, alternativePath, mainPathErr)
+	}
+
+	switch {
+	case mainPathErr == nil:
+		targetPath = mainPath
+	case alternativePathErr == nil:
+		targetPath = alternativePath
+	}
+
+	targetFile, err := os.Open(targetPath)
+	if err != nil {
+		err = fmt.Errorf("error while reading %s: %w", targetPath, err)
+
+		return err
+	}
+
+	_, err = io.Copy(writer, targetFile)
+	if err != nil {
+		return fmt.Errorf("error writing contents of %s to writer: %w", targetPath, err)
+	}
+
+	return nil
+}
+
+func fetchRemoteTemplate(kaex Kaex, w io.Writer, name string) error {
+	rawRepoURL, err := url.Parse(kaex.RepoURL)
+	if err != nil {
+		return fmt.Errorf("malformed url %s: %w", kaex.RepoURL, err)
+	}
+
+	rawRepoURL.Path = path.Join(rawRepoURL.Path, "examples")
+
+	mainURL, _ := url.Parse(rawRepoURL.String())
+	alternativeURL, _ := url.Parse(rawRepoURL.String())
+
+	mainURL.Path = path.Join(mainURL.Path, name)
+	alternativeURL.Path = path.Join(alternativeURL.Path, fmt.Sprintf("%s.yaml", name))
+
+	mainTemplate, mainErr := fetchURL(mainURL.String())
+	alternativeTemplate, alternativeErr := fetchURL(alternativeURL.String())
+	if mainErr != nil && alternativeErr != nil {
+		return fmt.Errorf("unable to fetch remote template: %w", mainErr)
+	}
+
+	switch {
+	case mainErr == nil:
+		fmt.Fprint(w, mainTemplate)
+	case alternativeErr == nil:
+		fmt.Fprint(w, alternativeTemplate)
+	}
+
+	return nil
+}
+
+func FetchTemplate(kaex Kaex, writer io.Writer, name string) error {
+	if kaex.ConfigPath != "" {
+		err := fetchLocalTemplate(kaex, writer, name)
+		if err == nil {
+			return nil
+		}
+	}
+
+	err := fetchRemoteTemplate(kaex, writer, name)
+	if err == nil {
+		return nil
+	}
+
+	return fmt.Errorf("unable to find template %s locally nor remotely: %w", name, err)
 }
