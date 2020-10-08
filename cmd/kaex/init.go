19c91f9e693e21874954880b0b599ce9fefc4e74
Julius Pedersen <deifyed@ctemplar.com>
2020-10-08 12:53:34 +0200

    👌 Improve help text for init command

--- cmd/kaex/init.go
+++ cmd/kaex/init.go
@@ -13,14 +13,21 @@ const templateNameHelp = `Define what template to use. Kaex will first look in ~
 https://github.com/oslokommune/kaex/tree/master/templates
 `
 
-const initializeHelpLong = `scaffolds a template. By default, kaex init will scaffold an application.yaml`
+const initializeHelpLong = `Scaffolds a template.
+
+By default, kaex init will scaffold an application.yaml. It will first look in your ~/.config/kaex/templates folder for
+an application.yaml file, then it will poll the /templates folder of the Kaex git repository.
+
+By putting files in ~/.config/kaex/templates, you can configure what Kaex will scaffold. This enables you to create
+different scaffolds for different environments and scaffold them for example by running kaex init -t <provider>.
+`
 
 func buildInitializeCommand(kaex api.Kaex) *cobra.Command {
 	cmd := &cobra.Command{
 		Use: "initialize",
 		Aliases: []string{"init", "i"},
-		Short: "scaffolds a template application.yaml",
-		Long: `scaffolds a template application.yaml.`,
+		Short: "scaffolds a template",
+		Long: initializeHelpLong,
 		RunE: func(_ *cobra.Command, args []string) error {
 			var buffer bytes.Buffer
 
